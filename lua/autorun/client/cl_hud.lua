842a20deb3cbd4163d8fd58644eaff0495e9f7d6
Baptiste Dupache <baptistedupache@gmail.com>
2023-05-03 10:47:29 +0200

    upgrade hud

--- lua/autorun/client/cl_hud.lua
+++ lua/autorun/client/cl_hud.lua
@@ -1,16 +1,28 @@
 include("autorun/sh_hudscp.lua")
 
 hook.Add("HUDPaint", "MonHUD", function()
-	local scrw,scrh = ScrW(), ScrH()
-    local ply = LocalPlayer()
-    local hp = ply:Health()
-    local maxhp = ply:GetMaxHealth()
-	local boxW = scrw * .1
-    local boxH = scrh * .02
+    -- start variables
+	local scrw,scrh = ScrW(), ScrH() -- screen size automatic for the player
+    local ply = LocalPlayer() -- player
+    local hp = ply:Health() -- health player
+    local maxhp = ply:GetMaxHealth() -- max health player
+	local boxW = scrw * .1 -- width 
+    local boxH = scrh * .02 -- height
+    local armor = ply:Armor()
+    local maxarmor = ply:GetMaxArmor()
+    --end variable
+
     -- start health
 	surface.SetDrawColor(0,0,0,200)
 	surface.DrawRect(scrw - boxW * 9.98, scrh - boxH * 1.1,  boxW, boxH)
     surface.SetDrawColor(255,0,0)
 	surface.DrawRect(scrw - boxW * 9.98, scrh - boxH * 1.1,  boxW * (hp / maxhp), boxH)
     -- end health
+    -- start armor
+    surface.SetDrawColor(0,0,0,200)
+	surface.DrawRect(scrw - boxW * 9.98, scrh - boxH * 2.2,  boxW, boxH)
+    surface.SetDrawColor(0,0,255)
+	surface.DrawRect(scrw - boxW * 9.98, scrh - boxH * 2.2,  boxW * (armor / maxarmor), boxH)
+    if armor = 0 then
+    -- end armor
 end) 
